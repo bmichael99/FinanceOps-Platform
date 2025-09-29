4ca8c6d96d1b0d23fd2b507a55be17a1b27641ed
Michael <michaelbrandmeyer99@gmail.com>
2025-09-29 14:41:07 -0700

    added todo's

--- Back End/controllers/invoiceController.js
+++ Back End/controllers/invoiceController.js
@@ -1,8 +1,18 @@
+const db = require('../repositories/invoiceRepository')
+
+
 function createInvoice(req, res) {
   console.log("hello");
   console.log(req.headers["content-type"])
-  console.log(req.files);
+  //console.log(req.files);
 
+  req.files.forEach(file => {
+    console.log(file);
+    //TODO: add current status, mimetype, filename, originalname, path, to prisma schema
+    //db.createUnprocessedInvoice({fileName: file.originalname, })
+  });
+
+  
   res.sendStatus(200);
 }
 
