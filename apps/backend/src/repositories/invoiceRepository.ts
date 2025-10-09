ca1e0aa40c5cdd95acaed8f2fad05b0358b88b4a
Michael <michaelbrandmeyer99@gmail.com>
2025-10-09 16:58:50 -0700

    clean up invoice repo functions, use  prisma types

--- apps/backend/src/repositories/invoiceRepository.ts
+++ apps/backend/src/repositories/invoiceRepository.ts
@@ -1,218 +1,23 @@
-import prisma from "./prisma.js";
+import prisma from "./prisma";
+import {InvoiceTable} from "../generated/prisma"
+import {UnprocessedInvoiceTable} from "../generated/prisma"
+import { Prisma } from '../generated/prisma'
 
-interface fieldConfidenceType {
-  fieldName : string,
-  confidence : number,
-}
-
-interface Invoice {
-  fileName : string,
-  projectName ?: string,
-  invoiceType ?: string,
-  paymentStatus ?: string,
-  fileIdSharepoint ?: string,
-  CustomerName ?: string,
-  CustomerId ?: string,
-  PurchaseOrder ?: string,
-  InvoiceId ?: string,
-  InvoiceDate ?: string,
-  DueDate ?: string,
-  VendorName ?: string,
-  VendorAddress ?: string,
-  VendorAddressRecipient ?: string,
-  CustomerAddress ?: string,
-  CustomerAddressRecipient ?: string,
-  BillingAddress ?: string,
-  BillingAddressRecipient ?: string,
-  ShippingAddress ?: string,
-  ShippingAddressRecipient ?: string,
-  SubTotal ?: string,
-  TotalDiscount ?: string,
-  TotalTax ?: string,
-  InvoiceTotal ?: string,
-  AmountDue ?: string,
-  PreviousUnpaidBalance ?: string,
-  RemittanceAddress ?: string,
-  RemittanceAddressRecipient ?: string,
-  ServiceAddress ?: string,
-  ServiceAddressRecipient ?: string,
-  ServiceStartDate ?: string,
-  ServiceEndDate ?: string,
-  VendorTaxId ?: string,
-  CustomerTaxId ?: string,
-  PaymentTerm ?: string,
-  fieldConfidences ?: fieldConfidenceType[],
-}
-
-interface invoiceTableType {
-  name ?: string, 
-  invoiceTableDataAsMarkdown : string,
-  rowCount ?: number,
-  columnCount ?: number,
-  invoiceId : number,
-}
-
-export const createInvoice = async ({
-  fileName,
-  projectName,
-  invoiceType,
-  paymentStatus,
-  fileIdSharepoint,
-  CustomerName,
-  CustomerId,
-  PurchaseOrder,
-  InvoiceId,
-  InvoiceDate,
-  DueDate,
-  VendorName,
-  VendorAddress,
-  VendorAddressRecipient,
-  CustomerAddress,
-  CustomerAddressRecipient,
-  BillingAddress,
-  BillingAddressRecipient,
-  ShippingAddress,
-  ShippingAddressRecipient,
-  SubTotal,
-  TotalDiscount,
-  TotalTax,
-  InvoiceTotal,
-  AmountDue,
-  PreviousUnpaidBalance,
-  RemittanceAddress,
-  RemittanceAddressRecipient,
-  ServiceAddress,
-  ServiceAddressRecipient,
-  ServiceStartDate,
-  ServiceEndDate,
-  VendorTaxId,
-  CustomerTaxId,
-  PaymentTerm
- } : Invoice) => {
+export const createInvoice = async (invoiceData : Prisma.InvoiceCreateInput) => {
+    
     const invoice = await prisma.invoice.create({
       data: {
-        fileName,
-        projectName,
-        invoiceType,
-        paymentStatus,
-        fileIdSharepoint,
-        CustomerName,
-        CustomerId,
-        PurchaseOrder,
-        InvoiceId,
-        InvoiceDate,
-        DueDate,
-        VendorName,
-        VendorAddress,
-        VendorAddressRecipient,
-        CustomerAddress,
-        CustomerAddressRecipient,
-        BillingAddress,
-        BillingAddressRecipient,
-        ShippingAddress,
-        ShippingAddressRecipient,
-        SubTotal,
-        TotalDiscount,
-        TotalTax,
-        InvoiceTotal,
-        AmountDue,
-        PreviousUnpaidBalance,
-        RemittanceAddress,
-        RemittanceAddressRecipient,
-        ServiceAddress,
-        ServiceAddressRecipient,
-        ServiceStartDate,
-        ServiceEndDate,
-        VendorTaxId,
-        CustomerTaxId,
-        PaymentTerm
+        ...invoiceData,
       },
     });
 
     return invoice;
 }
 
-export const createUnprocessedInvoice = async ({
-  fileName,
-  projectName,
-  invoiceType,
-  paymentStatus,
-  fileIdSharepoint,
-  CustomerName,
-  CustomerId,
-  PurchaseOrder,
-  InvoiceId,
-  InvoiceDate,
-  DueDate,
-  VendorName,
-  VendorAddress,
-  VendorAddressRecipient,
-  CustomerAddress,
-  CustomerAddressRecipient,
-  BillingAddress,
-  BillingAddressRecipient,
-  ShippingAddress,
-  ShippingAddressRecipient,
-  SubTotal,
-  TotalDiscount,
-  TotalTax,
-  InvoiceTotal,
-  AmountDue,
-  PreviousUnpaidBalance,
-  RemittanceAddress,
-  RemittanceAddressRecipient,
-  ServiceAddress,
-  ServiceAddressRecipient,
-  ServiceStartDate,
-  ServiceEndDate,
-  VendorTaxId,
-  CustomerTaxId,
-  PaymentTerm,
-  fieldConfidences = [],
- } : Invoice) => {
+export const createUnprocessedInvoice = async (unprocessedInvoiceData : Prisma.UnprocessedInvoiceCreateWithoutInvoiceTablesInput) => {
     const unprocessedInvoice = await prisma.unprocessedInvoice.create({
       data: {
-        fileName,
-        projectName,
-        invoiceType,
-        paymentStatus,
-        fileIdSharepoint,
-        CustomerName,
-        CustomerId,
-        PurchaseOrder,
-        InvoiceId,
-        InvoiceDate,
-        DueDate,
-        VendorName,
-        VendorAddress,
-        VendorAddressRecipient,
-        CustomerAddress,
-        CustomerAddressRecipient,
-        BillingAddress,
-        BillingAddressRecipient,
-        ShippingAddress,
-        ShippingAddressRecipient,
-        SubTotal,
-        TotalDiscount,
-        TotalTax,
-        InvoiceTotal,
-        AmountDue,
-        PreviousUnpaidBalance,
-        RemittanceAddress,
-        RemittanceAddressRecipient,
-        ServiceAddress,
-        ServiceAddressRecipient,
-        ServiceStartDate,
-        ServiceEndDate,
-        VendorTaxId,
-        CustomerTaxId,
-        PaymentTerm,
-        fieldConfidences: {
-          create: fieldConfidences.map(({ fieldName, confidence }) => ({
-            fieldName,
-            confidence,
-          })),
-        }
+        ...unprocessedInvoiceData,
       },
     });
 
@@ -220,173 +25,29 @@ export const createUnprocessedInvoice = async ({
   
 }
 
-export const updateInvoice = async ({
-  fileName,
-  projectName,
-  invoiceType,
-  paymentStatus,
-  fileIdSharepoint,
-  CustomerName,
-  CustomerId,
-  PurchaseOrder,
-  InvoiceId,
-  InvoiceDate,
-  DueDate,
-  VendorName,
-  VendorAddress,
-  VendorAddressRecipient,
-  CustomerAddress,
-  CustomerAddressRecipient,
-  BillingAddress,
-  BillingAddressRecipient,
-  ShippingAddress,
-  ShippingAddressRecipient,
-  SubTotal,
-  TotalDiscount,
-  TotalTax,
-  InvoiceTotal,
-  AmountDue,
-  PreviousUnpaidBalance,
-  RemittanceAddress,
-  RemittanceAddressRecipient,
-  ServiceAddress,
-  ServiceAddressRecipient,
-  ServiceStartDate,
-  ServiceEndDate,
-  VendorTaxId,
-  CustomerTaxId,
-  PaymentTerm
- } : Invoice) => {
+export const updateInvoice = async ({fileName, ...invoiceData} : Prisma.InvoiceCreateInput) => {
 
     const invoice = await prisma.invoice.update({
       where: {fileName},
-      data: {
-        fileName,
-        projectName,
-        invoiceType,
-        paymentStatus,
-        fileIdSharepoint,
-        CustomerName,
-        CustomerId,
-        PurchaseOrder,
-        InvoiceId,
-        InvoiceDate,
-        DueDate,
-        VendorName,
-        VendorAddress,
-        VendorAddressRecipient,
-        CustomerAddress,
-        CustomerAddressRecipient,
-        BillingAddress,
-        BillingAddressRecipient,
-        ShippingAddress,
-        ShippingAddressRecipient,
-        SubTotal,
-        TotalDiscount,
-        TotalTax,
-        InvoiceTotal,
-        AmountDue,
-        PreviousUnpaidBalance,
-        RemittanceAddress,
-        RemittanceAddressRecipient,
-        ServiceAddress,
-        ServiceAddressRecipient,
-        ServiceStartDate,
-        ServiceEndDate,
-        VendorTaxId,
-        CustomerTaxId,
-        PaymentTerm
-      },
+      data: invoiceData,
     });
 
     return invoice;
   
 }
 
-export const updateUnprocessedInvoice = async ({
-  fileName,
-  projectName,
-  invoiceType,
-  paymentStatus,
-  fileIdSharepoint,
-  CustomerName,
-  CustomerId,
-  PurchaseOrder,
-  InvoiceId,
-  InvoiceDate,
-  DueDate,
-  VendorName,
-  VendorAddress,
-  VendorAddressRecipient,
-  CustomerAddress,
-  CustomerAddressRecipient,
-  BillingAddress,
-  BillingAddressRecipient,
-  ShippingAddress,
-  ShippingAddressRecipient,
-  SubTotal,
-  TotalDiscount,
-  TotalTax,
-  InvoiceTotal,
-  AmountDue,
-  PreviousUnpaidBalance,
-  RemittanceAddress,
-  RemittanceAddressRecipient,
-  ServiceAddress,
-  ServiceAddressRecipient,
-  ServiceStartDate,
-  ServiceEndDate,
-  VendorTaxId,
-  CustomerTaxId,
-  PaymentTerm
- } : Invoice) => {
+export const updateUnprocessedInvoice = async ({fileName, ...unprocessedInvoiceData} : Prisma.UnprocessedInvoiceCreateWithoutInvoiceTablesInput) => {
 
     const unprocessedInvoice = await prisma.unprocessedInvoice.update({
       where: {fileName},
-      data: {
-        fileName,
-        projectName,
-        invoiceType,
-        paymentStatus,
-        fileIdSharepoint,
-        CustomerName,
-        CustomerId,
-        PurchaseOrder,
-        InvoiceId,
-        InvoiceDate,
-        DueDate,
-        VendorName,
-        VendorAddress,
-        VendorAddressRecipient,
-        CustomerAddress,
-        CustomerAddressRecipient,
-        BillingAddress,
-        BillingAddressRecipient,
-        ShippingAddress,
-        ShippingAddressRecipient,
-        SubTotal,
-        TotalDiscount,
-        TotalTax,
-        InvoiceTotal,
-        AmountDue,
-        PreviousUnpaidBalance,
-        RemittanceAddress,
-        RemittanceAddressRecipient,
-        ServiceAddress,
-        ServiceAddressRecipient,
-        ServiceStartDate,
-        ServiceEndDate,
-        VendorTaxId,
-        CustomerTaxId,
-        PaymentTerm
-      },
+      data: unprocessedInvoiceData,
     });
 
     return unprocessedInvoice;
   
 }
 
-export const createInvoiceTable = async ({name, invoiceTableDataAsMarkdown, rowCount, columnCount, invoiceId} : invoiceTableType) => {
+export const createInvoiceTable = async ({name, invoiceTableDataAsMarkdown, rowCount, columnCount, invoiceId} : InvoiceTable) => {
 
     const invoiceTable = await prisma.invoiceTable.create({
     data: {name, invoiceTableDataAsMarkdown, rowCount, columnCount, invoiceId}
@@ -396,7 +57,7 @@ export const createInvoiceTable = async ({name, invoiceTableDataAsMarkdown, rowC
 
 }
 
-export const createUnprocessedInvoiceTable = async ({name, invoiceTableDataAsMarkdown, rowCount, columnCount, invoiceId} : invoiceTableType) => {
+export const createUnprocessedInvoiceTable = async ({name, invoiceTableDataAsMarkdown, rowCount, columnCount, invoiceId} : UnprocessedInvoiceTable) => {
 
     const unprocessedInvoiceTable = await prisma.unprocessedInvoiceTable.create({
     data: {name, invoiceTableDataAsMarkdown, rowCount, columnCount, invoiceId}
