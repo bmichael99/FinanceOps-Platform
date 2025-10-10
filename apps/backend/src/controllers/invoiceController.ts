30bfd97de00717390890d905abe41efd4402463e
Michael <michaelbrandmeyer99@gmail.com>
2025-10-09 17:01:12 -0700

    add bullmq queue for files that need to be processed

--- apps/backend/src/controllers/invoiceController.ts
+++ apps/backend/src/controllers/invoiceController.ts
@@ -1,18 +1,34 @@
-//import db from "../repositories/invoiceRepository";
-import {Request, Response} from "express";
-//import { File } from "multer";
+import * as db from "../repositories/invoiceRepository";
+import {NextFunction, Request, Response} from "express";
+import {Queue} from 'bullmq';
 
-export function createInvoice(_req : Request, res : Response) {
-  console.log("hello");
-  //console.log(req.headers["content-type"])
-  //console.log(req.files);
+export async function createInvoice(req : Request, res : Response, _next: NextFunction) { 
+  const files = req.files as Express.Multer.File[];
 
-  // req.files.forEach(file => {
-  //   console.log(file);
-  //   //TODO: add current status, mimetype, filename, originalname, path, to prisma schema
-  //   //db.createUnprocessedInvoice({fileName: file.originalname, })
-  // });
+  const results = await Promise.allSettled(files?.map((file : Express.Multer.File) => 
+    db.createUnprocessedInvoice({fileName: file.filename, originalFileName: file.originalname, mimeType: file.mimetype, filePath: file.path, currentProcessingStatus: 'PENDING'})
+  ))
 
-  
-  res.sendStatus(200);
+  const fileQueue = new Queue('FileProcessing');
+  const successes : {file: string, originalFileName: string}[] = [];
+  const failures: {originalFileName: string, error: string}[] = [];
+
+  for(const [i,result] of results.entries()) {
+    if(result.status == "fulfilled"){
+      try{
+        await fileQueue.add(files[i].filename, {fileName: files[i].filename}, {removeOnComplete: true, removeOnFail: true}); 
+        successes.push({file: files[i].filename, originalFileName: files[i].originalname});
+      }
+      catch(queueErr) {
+        failures.push({originalFileName: files[i].originalname, error: `Queueing failed: ${queueErr}`})
+      }
+    } else {
+      failures.push({originalFileName: files[i].originalname, error: String(result.reason)})
+    }
+  }
+
+
+
+  //return to user which files succeeded and failed. Failed only needs original name for UX
+  res.status(200).json({successes, failures});
 }
