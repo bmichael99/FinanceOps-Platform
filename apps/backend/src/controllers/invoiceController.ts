370a45f36259f59c1d068a1ded8fbf33dfe884f5
Michael <michaelbrandmeyer99@gmail.com>
2025-10-16 19:19:35 -0700

    chore

--- apps/backend/src/controllers/invoiceController.ts
+++ apps/backend/src/controllers/invoiceController.ts
@@ -1,22 +1,34 @@
 import * as db from "../repositories/invoiceRepository";
 import {NextFunction, Request, Response} from "express";
 import {Queue} from 'bullmq';
+import { type User } from '../generated/prisma'
+
+export interface fileProcessingData {
+    fileName: string,
+}
 
 export async function createInvoice(req : Request, res : Response, _next: NextFunction) { 
   const files = req.files as Express.Multer.File[];
+  if(!files || files.length == 0){
+    return res.status(400).json("No files submitted. That shouldn't even be possible.");
+  }
+
 
   const results = await Promise.allSettled(files?.map((file : Express.Multer.File) => 
-    db.createUnprocessedInvoice({fileName: file.filename, originalFileName: file.originalname, mimeType: file.mimetype, filePath: file.path, currentProcessingStatus: 'PENDING'})
+    db.createUnprocessedInvoice({user: { connect: { id: req.user!.id } },fileName: file.filename, originalFileName: file.originalname, mimeType: file.mimetype, filePath: file.path, currentProcessingStatus: 'PENDING'})
   ))
 
   const fileQueue = new Queue('FileProcessing');
   const successes : {file: string, originalFileName: string}[] = [];
   const failures: {originalFileName: string, error: string}[] = [];
 
+  
+
   for(const [i,result] of results.entries()) {
     if(result.status == "fulfilled"){
       try{
-        await fileQueue.add(files[i].filename, {fileName: files[i].filename}, {removeOnComplete: true, removeOnFail: true}); 
+        const fileData : fileProcessingData = {fileName: files[i].filename};
+        await fileQueue.add(files[i].filename, fileData, {removeOnComplete: true, removeOnFail: 25, attempts: 3, backoff: {type: "exponential", delay: 3000}}); 
         successes.push({file: files[i].filename, originalFileName: files[i].originalname});
       }
       catch(queueErr) {
