0bd14885d8581928106921af17fc79489f7eac87
Michael <michaelbrandmeyer99@gmail.com>
2025-10-31 22:36:44 -0700

    feat: Display status and filename of recent file uploads. WIP.

--- apps/frontend/src/pages/invoices/components/FileUploadStatus.tsx
+++ apps/frontend/src/pages/invoices/components/FileUploadStatus.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import { TableCell, TableRow } from '@/components/ui/table';
+import React, { useState } from 'react'
+import { type FileResponseType } from '@finance-platform/types';
+import { type FileStatus } from "@finance-platform/types";
+
 
 type Props = {
-  
+  file: FileResponseType
 }
 
-function FileUploadStatus({}: Props) {
+function FileUploadStatus({file}: Props) {
+  // const [fileStatus, setFileStatus] = useState<FileStatus>("UPLOADING");
+
+
   return (
-    <div>FileUploadStatus</div>
+    <TableRow>
+      <TableCell>{file.status}</TableCell>
+      <TableCell>{file.originalFileName}</TableCell>
+    </TableRow>
   )
 }
 
