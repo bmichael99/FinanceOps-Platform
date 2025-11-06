b9e8c923c595493cb4df382a4950463f547c2750
Michael <michaelbrandmeyer99@gmail.com>
2025-11-05 23:40:02 -0800

    feat: moved to unprocessedInvoiceRouter

--- apps/backend/src/routes/invoiceRouter.ts
+++ apps/backend/src/routes/invoiceRouter.ts
@@ -1,11 +1,10 @@
 import {Router} from "express";
-import * as invoiceController from "../controllers/invoiceController.js";
-import passport from "passport";
-import uploadMiddleware from "../middlewares/uploadMiddleware.js";
+// import * as invoiceController from "../controllers/invoiceController.js";
+// import passport from "passport";
+// import uploadMiddleware from "../middlewares/uploadMiddleware.js";
 const invoiceRouter = Router();
 
 
-invoiceRouter.post("/invoices", passport.authenticate('jwt', {session: false}), uploadMiddleware, invoiceController.createInvoice);
 
 export default invoiceRouter;
 
