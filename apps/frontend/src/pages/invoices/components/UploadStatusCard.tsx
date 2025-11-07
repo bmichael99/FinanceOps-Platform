8e8d47171ec66d3df8963728360c7105a687822c
Michael <michaelbrandmeyer99@gmail.com>
2025-11-06 19:42:06 -0800

    feat: Loading skeleton while data is fetching on page load.

--- apps/frontend/src/pages/invoices/components/UploadStatusCard.tsx
+++ apps/frontend/src/pages/invoices/components/UploadStatusCard.tsx
@@ -1,33 +1,51 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import FileUploadStatus from './FileUploadStatus'
 import {
   Table,
   TableBody,
   TableCaption,
+  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
 import { type FileResponseType } from '@finance-platform/types'
 import { type UploadedFileType } from '../UploadInvoicePage';
-
+import { Skeleton } from '@/components/ui/skeleton'
 
 type Props = {
-  uploadedFiles: UploadedFileType
+  uploadedFiles: UploadedFileType,
+  loading: boolean
 }
 
 type tableProps = {
-  uploadedFiles: UploadedFileType
+  uploadedFiles: UploadedFileType,
+  loading: boolean
 }
 
-// function DataTable({uploadedFiles} : tableProps){
+function LoadingSkeleton(){
+  return Array.from({length: 3}).map((_val, i) => (
+      <TableRow key={`skeleton-${i}`}>
+        <TableCell><Skeleton className="h-[20px] w-[100px]" /></TableCell>
+        <TableCell><Skeleton className="h-[20px] w-[300px]" /></TableCell>
+        <TableCell><Skeleton className="h-[20px] w-[50px]" /></TableCell>
+      </TableRow>
+  ))
+}
 
-// }
-
-
-
-function UploadStatusTable({uploadedFiles} : tableProps) {
+function UploadStatusTable({uploadedFiles, loading} : tableProps) {
+  const [currTime, setCurrTime] = useState(new Date());
+  // update component every 60 seconds.
+  useEffect(() => {
+    const refreshComponent = setInterval(() => {
+      setCurrTime(new Date());
+    }, 60000)
+    return (() => {
+      clearInterval(refreshComponent);
+    })
+  }, [])
+  
   return (
     <Table containerClassName='overflow-y-auto max-h-96'>
       <TableCaption>A list of your recent invoice uploads.</TableCaption>
@@ -39,15 +57,16 @@ function UploadStatusTable({uploadedFiles} : tableProps) {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {Object.values(uploadedFiles).sort((a,b) => (b.uploadTime.getTime() - a.uploadTime.getTime())).map((file: FileResponseType) => (
-          <FileUploadStatus key= {file.fileName} file={file}/>
+        {loading ? <LoadingSkeleton /> :
+        Object.values(uploadedFiles).sort((a,b) => (b.uploadTime.getTime() - a.uploadTime.getTime())).map((file: FileResponseType) => (
+          <FileUploadStatus key={file.fileName} file={file}/>
         ))}
       </TableBody>
     </Table>
   )
 }
 
-function UploadStatusCard({uploadedFiles}: Props) {
+function UploadStatusCard({uploadedFiles, loading}: Props) {
   return (
       <Card className='w-full max-w-6xl mb-4'>
         <CardHeader >
@@ -55,7 +74,7 @@ function UploadStatusCard({uploadedFiles}: Props) {
           <CardDescription>View status of recent uploads. (24hrs)</CardDescription>
         </CardHeader>
         <CardContent>
-          <UploadStatusTable uploadedFiles={uploadedFiles}/>
+          <UploadStatusTable uploadedFiles={uploadedFiles} loading={loading}/>
         </CardContent>
       </Card>
   )
