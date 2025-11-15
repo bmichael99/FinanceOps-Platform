9032a5615bbbbc2fc6f5974b93a4666261e5efec
Michael <michaelbrandmeyer99@gmail.com>
2025-11-14 16:10:15 -0800

    Chore: wording of a comment

--- apps/backend/src/controllers/unprocessedInvoiceController.ts
+++ apps/backend/src/controllers/unprocessedInvoiceController.ts
@@ -97,7 +97,7 @@ export async function unprocessedInvoiceEvents(req: Request, res : Response){
 
 
 /**
- * Parameter options: 
+ * Query Parameter options: 
  * since: Date object that's received as a string, returns all unprocessed invoices greater than or equal to the date time provided. ex: ?since=new Date(Date.now() - 1000*60*60*24) will return unprocessed invoices within the last 24 hours
  * status: currentProcessingStatus filter, this is defined by the FileStatusType in the shared types. Current options are: UPLOADING, PENDING, PROCESSING, SAVING, COMPLETED, FAILED
  * view: sets the shape of the return data, determining what data is returned. Current options are summary: returns invoices as FileResponseType[] (shared type which includes fileName, originalFileName, status, and uploadTime(createdAt)). full: returns invoices as Invoice[] (all data returned by db, the full model for unprocessed invoice). custom: returns the specified fields in fields query param only.
