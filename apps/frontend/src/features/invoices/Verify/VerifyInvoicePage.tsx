41e3891053e3c855af4ed1d6780f8d2f8f07c1e3
Michael <michaelbrandmeyer99@gmail.com>
2025-11-14 16:07:32 -0800

    feat: init verify invoice page, WIP. Get unprocessed invoices.

--- apps/frontend/src/features/invoices/Verify/VerifyInvoicePage.tsx
+++ apps/frontend/src/features/invoices/Verify/VerifyInvoicePage.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import useFetchPrivate from '@/hooks/useFetchPrivate'
+import React, { useEffect, useState } from 'react'
 
 type Props = {}
 
 function VerifyInvoicePage({}: Props) {
+  const [invoiceName, setInvoiceName] = useState<string[]>([]);
+  const fetchPrivate = useFetchPrivate();
+  useEffect(() => {
+
+    async function getUnprocessedInvoiceNames(){
+      const view = "CUSTOM";
+      const fields = "mimeType,originalFileName,fileName";
+      const response = await fetchPrivate({endpoint: `/unprocessed-invoices?view=${view}&fields=${fields}`, method: "GET"});
+      const data = await response.json();
+      console.log(data);
+
+    }
+
+    getUnprocessedInvoiceNames();
+
+  },[])
+
+
   return (
     <div>VerifyInvoicePage</div>
   )
