2fe7cf2d03d776f5342fdba16cad7b9edcaec239
Michael <michaelbrandmeyer99@gmail.com>
2025-11-19 19:01:18 -0800

    feat: display invoice file with signed s3 url.

---
+++ apps/frontend/src/features/invoices/Verify/VerifyInvoiceDisplayFile.tsx
@@ -0,0 +1,41 @@
+import useFetchPrivate from '@/hooks/useFetchPrivate'
+import React, { useEffect, useState } from 'react'
+
+type Props = {
+  invoiceId: string | undefined
+}
+
+type getSignedURLResponseType = {
+  signedURL: string,
+  wasCached: boolean
+}
+
+function VerifyInvoiceDisplayFile({invoiceId}: Props) {
+  const [signedURL, setSignedURL] = useState<string>();
+  const fetchPrivate = useFetchPrivate();
+  useEffect(() => {
+    if(invoiceId){
+      const controller = new AbortController();
+      async function fetchS3() {
+        const response = await fetchPrivate({endpoint: `/unprocessed-invoices/${invoiceId}/signed-url`, method: "GET", abortController: controller})
+        const data : getSignedURLResponseType = await response.json();
+        setSignedURL(data.signedURL);
+        console.log(data);
+      }
+
+      fetchS3();
+      return(() => {
+        controller.abort();
+      })
+    }
+  }, [invoiceId])
+
+  return (
+    signedURL &&
+    <iframe src={signedURL} className='min-w-full h-full'>
+
+    </iframe>
+  )
+}
+
+export default VerifyInvoiceDisplayFile
