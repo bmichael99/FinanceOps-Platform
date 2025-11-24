f9b66d226d50d2b73f75bb455162fe1f57ef4b7d
Michael <michaelbrandmeyer99@gmail.com>
2025-11-24 08:48:00 -0800

    feat: loading states for loading skeletons until data is ready

--- apps/frontend/src/features/invoices/Verify/VerifyInvoicePage.tsx
+++ apps/frontend/src/features/invoices/Verify/VerifyInvoicePage.tsx
@@ -7,6 +7,7 @@ import {
 import { type FileStatusType, type UnprocessedInvoice } from '@finance-platform/types'
 import VerifyInvoiceForm from './VerifyInvoiceForm'
 import VerifyInvoiceDisplayFile from './VerifyInvoiceDisplayFile'
+import { Spinner } from '@/components/ui/spinner'
 
 type Props = {}
 
@@ -20,36 +21,39 @@ function VerifyInvoicePage({}: Props) {
   const [invoiceList, setInvoiceList] = useState<GetUnprocessedInvoiceNamesResponseType[]>([]);
   const [selectedFile, setSelectedFile] = useState<string>();
   const [invoiceData, setInvoiceData] = useState<UnprocessedInvoice>();
-  const [loadingInvoiceData, setLoadingInvoiceData] = useState<boolean>(false);
+  const [loadingInvoiceData, setLoadingInvoiceData] = useState<boolean>(true);
+  const [loadingInvoiceList, setLoadingInvoiceList] = useState<boolean>(true);
   const fetchPrivate = useFetchPrivate();
   //fetch a list of unprocessed and completed invoices
   useEffect(() => {
     async function getUnprocessedInvoiceNames(){
+      setLoadingInvoiceList(true);
       const view = "CUSTOM";
       const fields = "mimeType,originalFileName,fileName";
       const status = "COMPLETED"
       const response = await fetchPrivate({endpoint: `/unprocessed-invoices?view=${view}&fields=${fields}&status=${status}`, method: "GET"});
       const data = await response.json();
       setInvoiceList(data);
+      setLoadingInvoiceList(false);
     }
     getUnprocessedInvoiceNames();
   },[])
 
   useEffect(() => {
-    if(selectedFile){
-      setLoadingInvoiceData(true);
-      const controller = new AbortController();
-      async function getUnprocessedInvoiceData() {
-        const response = await fetchPrivate({endpoint: `/unprocessed-invoices/${selectedFile}`, method: "GET", abortController: controller});
-        const data: UnprocessedInvoice = await response.json();
-        setInvoiceData(data);
-        setLoadingInvoiceData(false);
-      }
-      getUnprocessedInvoiceData();
-      return(() => {
-        controller.abort();
-      })
+    if(!selectedFile) return;
+
+    setLoadingInvoiceData(true);
+    const controller = new AbortController();
+    async function getUnprocessedInvoiceData() {
+      const response = await fetchPrivate({endpoint: `/unprocessed-invoices/${selectedFile}`, method: "GET", abortController: controller});
+      const data: UnprocessedInvoice = await response.json();
+      setInvoiceData(data);
+      setLoadingInvoiceData(false);
     }
+    getUnprocessedInvoiceData();
+    return(() => {
+      controller.abort();
+    })
   },[selectedFile])
 
   function handleInvoiceSelect(invoiceId : string) {
@@ -58,14 +62,15 @@ function VerifyInvoicePage({}: Props) {
 
   return (
   <div className='flex flex-col items-center h-svh'>
-    <div className='my-2'>
+    <div className='my-2 flex items-center gap-2'>
+      {loadingInvoiceList && <Spinner></Spinner>}
       <NativeSelect onChange={(e) => handleInvoiceSelect(e.target.value)}>
-        <NativeSelectOption value="">Select Invoice</NativeSelectOption>
+        <NativeSelectOption value="">{loadingInvoiceList ? "Loading.." : "Select Invoice"}</NativeSelectOption>
         {invoiceList.map(invoice => <NativeSelectOption key={invoice.fileName} value={invoice.fileName}>{invoice.originalFileName}</NativeSelectOption>)}
       </NativeSelect>
     </div>
     <div className='grid grid-cols-2 mb-2 w-full h-full gap-4'>
-      {loadingInvoiceData ? <p>Change to loading skeleton</p> : ((selectedFile && invoiceData) && <VerifyInvoiceForm invoiceId={selectedFile} invoiceData={invoiceData}></VerifyInvoiceForm>)}
+      {selectedFile && (!invoiceData ? <p>Change to loading skeleton</p> : <VerifyInvoiceForm invoiceId={selectedFile} invoiceData={invoiceData}></VerifyInvoiceForm>)}
       {selectedFile && <VerifyInvoiceDisplayFile invoiceId={selectedFile}></VerifyInvoiceDisplayFile>}
     </div>
   </div>
