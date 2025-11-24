327233c8558e7925c4622648f83469abf56fd18e
Michael <michaelbrandmeyer99@gmail.com>
2025-11-24 11:00:33 -0800

    feat: Loading skeleton for signed URL

--- apps/frontend/src/features/invoices/Verify/VerifyInvoiceDisplayFile.tsx
+++ apps/frontend/src/features/invoices/Verify/VerifyInvoiceDisplayFile.tsx
@@ -1,3 +1,4 @@
+import { Skeleton } from '@/components/ui/skeleton'
 import useFetchPrivate from '@/hooks/useFetchPrivate'
 import React, { useEffect, useState } from 'react'
 
@@ -12,13 +13,16 @@ type getSignedURLResponseType = {
 
 function VerifyInvoiceDisplayFile({invoiceId}: Props) {
   const [signedURL, setSignedURL] = useState<string>();
+  const [loadingSignedURL, setLoadingSignedURL] = useState<boolean>(true);
   const fetchPrivate = useFetchPrivate();
   useEffect(() => {
     const controller = new AbortController();
     async function fetchSignedURL() {
+      setLoadingSignedURL(true);
       const response = await fetchPrivate({endpoint: `/unprocessed-invoices/${invoiceId}/signed-url`, method: "GET", abortController: controller})
       const data : getSignedURLResponseType = await response.json();
       setSignedURL(data.signedURL);
+      setLoadingSignedURL(false);
       console.log(data);
     }
 
@@ -29,10 +33,12 @@ function VerifyInvoiceDisplayFile({invoiceId}: Props) {
   }, [invoiceId])
 
   return (
-    signedURL &&
-    <iframe src={signedURL} className='min-w-full h-full'>
-
-    </iframe>
+  <div>
+    {loadingSignedURL ?
+    <Skeleton className='h-full w-full'></Skeleton> :
+    <iframe src={signedURL} className='min-w-full h-full'></iframe>
+    }
+  </div>
   )
 }
 
