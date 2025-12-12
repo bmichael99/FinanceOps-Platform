b54822511907efb2fe37ada364b96a34e3810f76
Michael <michaelbrandmeyer99@gmail.com>
2025-12-12 14:15:42 -0800

    optimization: make db calls without waiting.

--- apps/backend/src/services/invoiceService.ts
+++ apps/backend/src/services/invoiceService.ts
@@ -8,6 +8,9 @@ type inputs = {
 
 
 //returns sums of invoices grouped by months, ex: last 6 months
+//need to make this function again but grouped by days.
+//TODO: We need an option to get all ranges of dates, in this case we should fetch starting from the oldest date in the db. 
+//TODO: It could be much better to use a raw sql query that does everything here for me? if it exists?
 export async function getLastInvoiceSumsGroupedByMonth({monthCount, userId, }: inputs){
   const currDate = new Date();
 
@@ -19,24 +22,27 @@ export async function getLastInvoiceSumsGroupedByMonth({monthCount, userId, }: i
       currentYear -= 1;
       currentMonth = 12;
     }
-    const monthlyRevenue = await db.getAllInvoicesUsingAggregate({
+    
+    const monthlyRevenuePromise = db.getAllInvoicesUsingAggregate({
       where: {userId , DueDate: {gte: new Date(Date.UTC(currentYear,currentMonth,1,0,0,0,1)), lte: new Date(Date.UTC(currentYear,currentMonth+1,1,0,0,0,0))}, verificationStatus: "VERIFIED", paymentStatus: "PAID", invoiceType: "ACCOUNTS_RECEIVABLE"},
       _sum: {
         InvoiceTotal: true,
       }
     });
-    const monthlyRevenueOwed = await db.getAllInvoicesUsingAggregate({
+    const monthlyRevenueOwedPromise = db.getAllInvoicesUsingAggregate({
       where: {userId , DueDate: {gte: new Date(Date.UTC(currentYear,currentMonth,1,0,0,0,1)), lte: new Date(Date.UTC(currentYear,currentMonth+1,1,0,0,0,0))}, verificationStatus: "VERIFIED", paymentStatus: "UNPAID", invoiceType: "ACCOUNTS_RECEIVABLE"},
       _sum: {
         InvoiceTotal: true,
       }
     });
-    const monthlyExpenditure = await db.getAllInvoicesUsingAggregate({
+    const monthlyExpenditurePromise = db.getAllInvoicesUsingAggregate({
       where: {userId , DueDate: {gte: new Date(Date.UTC(currentYear,currentMonth,1,0,0,0,1)), lte: new Date(Date.UTC(currentYear,currentMonth+1,1,0,0,0,0))}, verificationStatus: "VERIFIED", paymentStatus: "PAID", invoiceType: "ACCOUNTS_PAYABLE"},
       _sum: {
         InvoiceTotal: true,
       }
     });
+
+    const [monthlyRevenue, monthlyRevenueOwed, monthlyExpenditure] = await Promise.all([monthlyRevenuePromise,monthlyRevenueOwedPromise,monthlyExpenditurePromise])
 
     // const v = (t:any) => t._sum?.InvoiceTotal ?? 0;
 
