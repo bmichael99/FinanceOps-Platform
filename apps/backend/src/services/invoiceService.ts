d7d70dcba62a51470de5b4578eb31e603db9e6fa
Michael <michaelbrandmeyer99@gmail.com>
2026-03-12 19:02:22 -0700

    feat: Schema, Data, UI update
    added getLastInvoiceSumsGroupedByDay, a data fetching function for graphs that groups by day.

--- apps/backend/src/services/invoiceService.ts
+++ apps/backend/src/services/invoiceService.ts
@@ -1,20 +1,24 @@
 import * as db from "../repositories/invoiceRepository";
-import { Invoice, InvoiceMonthlyChartData } from "@finance-platform/types";
+import { Invoice, InvoiceChartData, InvoiceChartTypes } from "@finance-platform/types";
 
-type inputs = {
+type monthInputs = {
   monthCount: number,
   userId: number,
 }
 
+type dayInputs = {
+  dayCount: number,
+  userId: number,
+}
 
 //returns sums of invoices grouped by months, ex: last 6 months
 //need to make this function again but grouped by days.
 //TODO: We need an option to get all ranges of dates, in this case we should fetch starting from the oldest date in the db. 
 //TODO: It could be much better to use a raw sql query that does everything here for me? if it exists?
-export async function getLastInvoiceSumsGroupedByMonth({monthCount, userId, }: inputs){
+export async function getLastInvoiceSumsGroupedByMonth({monthCount, userId, }: monthInputs){
   const currDate = new Date();
 
-  const invoiceData : InvoiceMonthlyChartData[] = [];
+  const invoiceData : InvoiceChartData[] = [];
   let currentYear = currDate.getFullYear();
   let currentMonth = currDate.getMonth();
   for(let i = 0; i < monthCount; i++){
@@ -48,16 +52,68 @@ export async function getLastInvoiceSumsGroupedByMonth({monthCount, userId, }: i
     // const v = (t:any) => t._sum?.InvoiceTotal ?? 0;
     
     invoiceData.push({
-      year: currentYear,
-      month: currentMonth+1,
+      // year: currentYear,
+      // month: currentMonth+1,
       formattedDate: new Date(currentYear,currentMonth).toLocaleString('default', {month: 'short'}) + " " + currentYear,
       revenue: monthlyRevenue._sum?.InvoiceTotal ?? 0,
-      projectedRevenue: monthlyRevenueOwed._sum?.InvoiceTotal ?? 0,
+      // projectedRevenue: monthlyRevenueOwed._sum?.InvoiceTotal ?? 0,
       expenditure: monthlyExpenditure._sum?.InvoiceTotal ?? 0,
       profit: (monthlyRevenue._sum?.InvoiceTotal ?? 0) - (monthlyExpenditure._sum?.InvoiceTotal ?? 0),
     });
     //increment current month
     currentMonth--;
+  }
+  invoiceData.reverse();
+  return invoiceData;
+}
+
+export async function getLastInvoiceSumsGroupedByDay({dayCount, userId, }: dayInputs){
+  let currDate = new Date();
+
+  const invoiceData : InvoiceChartData[] = [];
+  for(let i = 0; i < dayCount; i++){
+    //currentMonth = monthIndex which is a number from 0-11 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date
+    let currentYear = currDate.getFullYear();
+    let currentMonth = currDate.getMonth();
+    let currentDay = currDate.getDay();
+    
+    const dailyRevenuePromise = db.getAllInvoicesUsingAggregate({
+      where: {userId , DueDate: {gte: new Date(Date.UTC(currentYear,currentMonth,currentDay,0,0,0,1)), lte: new Date(Date.UTC(currentYear,currentMonth,currentDay+1,0,0,0,0))}, verificationStatus: "VERIFIED", paymentStatus: "PAID", invoiceType: "ACCOUNTS_RECEIVABLE"},
+      _sum: {
+        InvoiceTotal: true,
+      }
+    });
+    const dailyRevenueOwedPromise = db.getAllInvoicesUsingAggregate({
+      where: {userId , DueDate: {gte: new Date(Date.UTC(currentYear,currentMonth,currentDay,0,0,0,1)), lte: new Date(Date.UTC(currentYear,currentMonth,currentDay+1,0,0,0,0))}, verificationStatus: "VERIFIED", paymentStatus: "UNPAID", invoiceType: "ACCOUNTS_RECEIVABLE"},
+      _sum: {
+        InvoiceTotal: true,
+      }
+    });
+    const dailyExpenditurePromise = db.getAllInvoicesUsingAggregate({
+      where: {userId , DueDate: {gte: new Date(Date.UTC(currentYear,currentMonth,currentDay,0,0,0,1)), lte: new Date(Date.UTC(currentYear,currentMonth,currentDay+1,0,0,0,0))}, verificationStatus: "VERIFIED", paymentStatus: "PAID", invoiceType: "ACCOUNTS_PAYABLE"},
+      _sum: {
+        InvoiceTotal: true,
+      }
+    });
+
+    const [dailyRevenueP, dailyRevenueOwedP, dailyExpenditureP] = await Promise.all([dailyRevenuePromise,dailyRevenueOwedPromise,dailyExpenditurePromise])
+    const dailyRevenue = dailyRevenueP._sum?.InvoiceTotal;
+    const dailyRevenueOwed = dailyRevenueOwedP._sum?.InvoiceTotal;
+    const dailyExpenditure = dailyExpenditureP._sum?.InvoiceTotal;
+
+    // const v = (t:any) => t._sum?.InvoiceTotal ?? 0;
+    
+    if(dailyRevenue || dailyExpenditure){
+      invoiceData.push({
+        formattedDate: new Date(currentYear,currentMonth).toLocaleString('default', {month: 'short'}) + " " + currentDay,
+        revenue: dailyRevenue ?? 0,
+        expenditure: dailyExpenditure ?? 0,
+        profit: (dailyRevenue ?? 0) - (dailyExpenditure ?? 0),
+      });
+    } 
+    
+    //decrease date by a day
+    currDate = new Date(currDate.getTime() - 24*60*60*1000);
   }
   invoiceData.reverse();
   return invoiceData;
