b95350a560f0236893945cad9af6d6fd6bd22d13
Michael <michaelbrandmeyer99@gmail.com>
2026-03-18 20:32:50 -0700

    chore: removed unused code

--- apps/frontend/src/features/invoices/Browse/BrowseInvoiceDataTable.tsx
+++ apps/frontend/src/features/invoices/Browse/BrowseInvoiceDataTable.tsx
@@ -106,24 +106,6 @@ function BrowseInvoiceDataTable<TData, TValue>({columns, data}: DataTableProps<T
         </Table>
       </div>
       {/*Pagination Controls*/}
-      {/* <div className="flex items-center justify-end space-x-2 py-4">
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          Previous
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          Next
-        </Button>
-      </div> */}
       <DataTablePagination table={table}/>
     </div>
   )
