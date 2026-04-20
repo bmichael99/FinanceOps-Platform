994ee8f9796bba4455566d495e7cf6e70035aa3f
Michael <michaelbrandmeyer99@gmail.com>
2026-04-20 16:50:50 -0700

    feat: cleaned up google sign in and now correctly redirects to backend api endpoint

--- apps/frontend/src/pages/auth/GoogleLogInButton.tsx
+++ apps/frontend/src/pages/auth/GoogleLogInButton.tsx
@@ -1,20 +1,19 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 type Props = {}
+
 
 function GoogleLogInButton({}: Props) {
   const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+  const API_URL = import.meta.env.VITE_API_URL;
 
-  function onSignIn(){
-    console.log("yooo....");
-  }
   return (
     <>
       <div id="g_id_onload"
         data-client_id={`${CLIENT_ID}.apps.googleusercontent.com`}
         data-context="signin"
         data-ux_mode="popup"
-        data-callback="onSignIn"
+        data-login_uri={`${API_URL}/auth/google/receiver`}
         data-auto_prompt="false">
     </div>
 
