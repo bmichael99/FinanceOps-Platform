16ee527657563e76f5f9a2e8e74dfbd1c4c3dfde
Michael <michaelbrandmeyer99@gmail.com>
2026-04-24 16:26:14 -0700

    feat: Docker integration, WIP.
    worker split into an access point file and the actual worker object and logic file

--- apps/backend/src/workers/fileProcessor.ts
+++ apps/backend/src/workers/fileProcessor.ts
@@ -1,97 +1,53 @@
 import path from "path";
 import dotenv from 'dotenv';
 dotenv.config();
-import {type Job, JobProgress, Worker} from 'bullmq';
+import {type Job, Worker} from 'bullmq';
 import { processDocumentData } from '../integrations/mockController';
 import { type fileProcessingData } from '../controllers/invoiceController';
 import * as db from "../repositories/invoiceRepository";
 import IORedis from 'ioredis';
 import { uploadFile } from '../integrations/S3AWS';
-
 import fs from "fs";
 import { delay } from '../utils/delay';
-
-const connection = new IORedis({ host: process.env.REDIS_HOST, port: Number(process.env.REDIS_PORT), maxRetriesPerRequest: null  });
-
-const fileProcessor = new Worker('FileProcessing', async (job : Job) => {
-  const data : fileProcessingData = job.data;
-
-  await db.updateInvoice(data.userId, data.fileName, {currentProcessingStatus: "PROCESSING"})
-  job.updateProgress("PROCESSING");
-
-  //send file to get processed by azure, then save extracted data to database. Also update status.
-  // ### UNCOMMENT BELOW IN PROD. KEEPING COSTS DOWN BY NOT INCLUDING THIS IN DEV FOR NOW###
-  const extractedData = await processDocumentData(data.fileName);
-  console.log(extractedData);
-
-  const storeInvoice = await db.updateInvoice(data.userId, data.fileName, {currentProcessingStatus: "SAVING" , ...extractedData});
-  job.updateProgress("SAVING");
-
-  // //send file to s3 after processing is finished.
-  const filePath = path.join(__dirname, "../../uploads/", data.fileName);
-  await uploadFile(data.fileName, filePath, storeInvoice.mimeType);
-  await fs.promises.unlink(filePath);
-  await delay(1500); //for testing only, remove when uncommenting everything else.
-
-  // return s3Response;
-  console.log(`Dev Build, Processed: ${data.fileName}`)
-  job.updateProgress("COMPLETED");
-},
-{
-  connection,
-  concurrency: 1,
-  limiter: {max: 1, duration: 3000},}
-);
-
-fileProcessor.on('completed', async (job: Job, _returnValue: any) => {
-  const data : fileProcessingData = job?.data;
-  await db.updateInvoice(data.userId, data.fileName, {currentProcessingStatus: "COMPLETED"});
-})
-
-fileProcessor.on('failed', async (job: Job | undefined, error: Error, _prev: string) => {
-  const data : fileProcessingData = job?.data;
-  await db.updateInvoice(data.userId, data.fileName, {currentProcessingStatus: "FAILED"});
-  console.error(error.message);
-});
-
-fileProcessor.on('progress', (job: Job, progress: JobProgress) => {
-  const data : fileProcessingData = job?.data;
-  // const res = getClient(data.userId);
-
-  const payload: fileProcessingData = {userId: data.userId, fileName: data.fileName, originalFileName: data.originalFileName, uploadTime: data.uploadTime, status: progress};
-  connection.publish("FileProcessing", JSON.stringify(payload));
-});
+import {config} from "../config/config";
 
 
-//error listener to avoid NodeJS raising an unhandled exception when an error occurs.
-fileProcessor.on('error', err => {
-  // log the error
-  console.error(err);
-});
+export function createWorker(){
+  const connection = new IORedis({ host: process.env.REDIS_HOST, port: Number(process.env.REDIS_PORT), maxRetriesPerRequest: null  });
+  const publisher = new IORedis({ host: process.env.REDIS_HOST, port: Number(process.env.REDIS_PORT), maxRetriesPerRequest: null  });
 
-//graceful shutdown logic
-const shutdown = async (signal: string) => {
-  console.log(`Received ${signal}. Shutting down gracefully...`);
+  const fileProcessor = new Worker('FileProcessing', async (job : Job) => {
+    const data : fileProcessingData = job.data;
 
-  try {
-    //stop taking new jobs and wait for current jobs to finish
-    await fileProcessor.close();
-    console.log('Worker closed.');
+    await db.updateInvoice(data.userId, data.fileName, {currentProcessingStatus: "PROCESSING"})
+    await job.updateProgress("PROCESSING");
 
-    //close Redis connection
-    await connection.quit();
-    console.log('Redis connection closed.');
+    //send file to get processed by azure, then save extracted data to database. Also update status.
+    const extractedData = await processDocumentData(data.fileName);
+    console.log(extractedData);
 
-    process.exit(0);
-  } catch (err) {
-    console.error('Error during shutdown:', err);
-    process.exit(1);
-  }
-};
+    const storeInvoice = await db.updateInvoice(data.userId, data.fileName, {currentProcessingStatus: "SAVING" , ...extractedData});
+    await job.updateProgress("SAVING");
 
-// Handle SIGINT (Ctrl+C) and SIGTERM (e.g., Docker stop)
-process.on('SIGINT', () => shutdown('SIGINT'));
-process.on('SIGTERM', () => shutdown('SIGTERM'));
+    //TODO:API uploads straight to S3 (or a staging bucket), then enqueues the job with the S3 key. 
+    //The worker downloads from S3, processes, uploads result, deletes staging. This makes your workers truly stateless and horizontally scalable.
+    // //send file to s3 after processing is finished.
+    const filePath = path.join(config.UPLOADS_DIR, data.fileName);
+    await uploadFile(data.fileName, filePath, storeInvoice.mimeType);
+    await fs.promises.unlink(filePath);
+    await delay(1500); //for testing only, remove when uncommenting everything else.
 
-//fileProcessor.run();
+    // return s3Response;
+    console.log(`Dev Build, Processed: ${data.fileName}`)
+    await job.updateProgress("COMPLETED");
+  },
+  {
+    connection,
+    concurrency: 1,
+    limiter: {max: 1, duration: 3000},
+  });
+
+  return  {fileProcessor, connection, publisher};
+}
+
 
