f8f247bf4d944de9afb82157fb665a40c4f06aa7
Michael <michaelbrandmeyer99@gmail.com>
2026-04-26 20:44:40 -0700

    fix: edited migration to add chatbot_user if it doesn't exist. Was an issue because I originally created this role on my local machine instead of via a migration file

--- apps/backend/src/prisma/migrations/20251128190419_rls/migration.sql
+++ apps/backend/src/prisma/migrations/20251128190419_rls/migration.sql
@@ -1,6 +1,15 @@
 -- Enable Row Level Security
 ALTER TABLE "Invoice" ENABLE ROW LEVEL SECURITY;
 
+--create chatbot_user
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'chatbot_user') THEN
+    CREATE ROLE chatbot_user NOLOGIN;
+  END IF;
+END
+$$;
+
 -- Create Policies
 CREATE POLICY user_chatbot_policy
   ON "Invoice"
