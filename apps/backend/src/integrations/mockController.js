e8c20f6d10eaaa8607f86165d7e30822d7be7a89
Michael <michaelbrandmeyer99@gmail.com>
2026-04-26 20:56:58 -0700

    fix: use env for uploads directory in processDocumentData service.

--- apps/backend/src/integrations/mockController.js
+++ apps/backend/src/integrations/mockController.js
@@ -5,9 +5,9 @@ import * as db from "../repositories/invoiceRepository";
 import * as azure from "./Azure.js";
 import { stringToDate } from '../utils/dateParser';
 
-export async function processDocumentData(fileName){
+export async function processDocumentData(invoicePath, fileName){
   try{
-    const invoicePath = path.resolve(__dirname, "../../uploads",fileName);
+    // const invoicePath = path.resolve(config.UPLOADS_DIR,fileName);
     const rawData = await azure.getDocumentData(invoicePath);
     if(!rawData){
       throw new Error("Unable to extract document data with azure");
