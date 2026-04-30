6a8db5d6bd661a116c784e5a57480c2899b3a968
Michael <michaelbrandmeyer99@gmail.com>
2026-04-29 21:26:07 -0700

    refactor(routes): temp. Redirect / to /dashboard

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import { Button } from './components/ui/button'
 import DashboardLayout from './layouts/DashboardLayout'
 import './App.css'
-import { Link } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
 function App() {
 
 
   return (
-    <>
-    <Link to="/dashboard">Dashboard</Link>
-    <p>Home page</p>
-      </>
+    <Navigate to="/dashboard" replace />
+    // <>
+    // <Link to="/dashboard">Dashboard</Link>
+    // <p>Home page</p>
+    //   </>
   )
 }
 
