a30ab3635b249de8f7393d2752899592f307cf9b
Michael <michaelbrandmeyer99@gmail.com>
2026-05-27 14:20:37 -0700

    chore: increase capacity to 10 for auth rate limit

--- apps/backend/src/routes/authRouter.ts
+++ apps/backend/src/routes/authRouter.ts
@@ -4,10 +4,10 @@ import { rateLimiter } from "../middlewares/rateLimitMiddleware.js";
 const authRouter = Router();
 
 
-authRouter.post("/auth/register", rateLimiter({capacity: 5, refillRate: 1, refillInterval: 60, prefix: 'global'}), authController.registerUserPost);
-authRouter.post("/auth/login", rateLimiter({capacity: 5, refillRate: 1, refillInterval: 60, prefix: 'global'}), authController.logInUserPost);
-authRouter.get("/auth/log-out", rateLimiter({capacity: 5, refillRate: 1, refillInterval: 60, prefix: 'global'}), authController.handleLogout);
+authRouter.post("/auth/register", rateLimiter({capacity: 10, refillRate: 1, refillInterval: 60, prefix: 'auth'}), authController.registerUserPost);
+authRouter.post("/auth/login", rateLimiter({capacity: 10, refillRate: 1, refillInterval: 60, prefix: 'auth'}), authController.logInUserPost);
+authRouter.get("/auth/log-out", rateLimiter({capacity: 10, refillRate: 1, refillInterval: 60, prefix: 'auth'}), authController.handleLogout);
 //authRouter.get("/auth/me", requireAuth, authController.getCurrentUser);
-authRouter.post("/auth/google/receiver", rateLimiter({capacity: 5, refillRate: 1, refillInterval: 60, prefix: 'global'}), authController.handleGoogleAuth);
+authRouter.post("/auth/google/receiver", rateLimiter({capacity: 10, refillRate: 1, refillInterval: 60, prefix: 'auth'}), authController.handleGoogleAuth);
 
 export default authRouter;
